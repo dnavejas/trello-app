1e40fecba09648238696c2a0d9b15f53c64cd149
Danny Navejas <dannynavejas@gmail.com>
2019-06-19 18:09:46 -0500

    added index.html, css. and js for static public folder. ignoring node_modules folder and contents

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(express.static('static'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(express.json());
+app.use(express.urlencoded());
+
+let cards = [];
+
+let card1 = {"cardName": "test card 1", "cardDesc": "really cool card description"};
+let card2 = {"cardName": "test card 2", "cardDesc": "really cool card description"};
+
+cards.push(card1);
+cards.push(card2);
+
+app.post('/cards', function(req, res) {
+    console.log('we have a post');
+    console.log("card name", req.body.cardName);
+    console.log("card desc", req.body.cardDesc);
+    cards.push({"cardName": req.body.cardName, "cardDesc":req.body.cardDesc});
+    res.send("got it");
+});
+
+app.get('/cards', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(cards, null, 3));
+});
+
+app.get('/', function(req, res){
+    res.send("<h2>Hello World</h2>")
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
